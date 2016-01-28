f887adf76aecc9e838fb4f86ceb5156206ae846f
Craig Adam <craig@adam11.ca>
2016-01-28 07:08:58 +0000

    Finished version 1

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -5,15 +5,17 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Settings</div>
+                <div class="panel-heading">To do</div>
 
                 <div class="panel-body">
 
-                    <h3>To do list</h3>
+                    <h3>My list of things to do</h3>
                     <ul>
                         <li>Set which accounts are included in import (Currently hard-coded to MasterCard and Chequing)</li>
-                        <li>Manage rules for auto-assignment of categories</li>
+                        <li><del>Manage rules for auto-assignment of categories</del></li>
                         <li>Manage notifications for approaching/over budget</li>
+                        <li>Handle income categories as well</li>
+                        <li>Deal with duplicate incoming transactions</li>
                     </ul>
 
                 </div>
