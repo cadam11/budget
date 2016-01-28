fa0bc39d2efc29d374b7841f9894c6ab9385a4b1
Craig Adam <craig@adam11.ca>
2016-01-28 02:58:15 +0000

    Cleanup and more work

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -4,6 +4,8 @@ namespace Budget\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Excel;
+use Carbon\Carbon;
+use Budget\Transaction;
 use Budget\Http\Requests;
 use Budget\Http\Controllers\Controller;
 
@@ -55,9 +57,36 @@ class ImportController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function parse(Request $request){
+        if (!$request->session()->has('file')) return $this->index();
 
     	$csv = $request->session()->pull('file');
-    	if (!$csv) return $this->index();
+
+        $accounts = ['MasterCard', 'Chequing'];
+        $transactions = $csv->filter(function($item) use ($accounts) {
+            return in_array($item->account_type, $accounts);
+        });
+
+        $mapped = $transactions->map(function($item) {
+            $record = [
+                'date'                  => new Carbon($item->transaction_date),
+                'account'               => $item->account_type,
+                'description'           => ucwords(strtolower($item->description_1)),
+                'imported_description1' => $item->description_1,
+                'imported_description2' => $item->description_2,
+                'amount'                => $item->cad * -1,
+                'created_at'            => Carbon::now(),
+                'updated_at'            => Carbon::now(),
+            ];
+
+            // TODO: get category smartly
+            
+            
+            return $record;
+        });
+        
+        Transaction::insert($mapped->toArray());
+        $import = Transaction::orderby('created_at', 'desc')->limit($mapped->count())->get();
+        dd($import);
 
     	$grouped = $csv->groupBy('account_type');
     	dd($grouped);
