e98b81323327617134d6ba81c8c09cc3b8d4c1ac
Craig Adam <craig@adam11.ca>
2016-02-17 09:08:14 -0600

    Fixed dropdown bug and added page titles

--- resources/views/budgets/create.blade.php
+++ resources/views/budgets/create.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('pagetitle', 'Create Budget')
 @section('content')
 <div class="container">
     <div class="row">
--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('pagetitle', 'Transactions')
 @section('content')
 <div class="container">
     <div class="row">
